0e74b0db5ad61561ec9464c94cda80a29f99a9fd
Rajdeep Chatterjee <rajdeep.chatterjee1902@gmail.com>
2024-05-16 00:25:22 +0530

    Navbar Changes

--- static/components/Navbar.js
+++ static/components/Navbar.js
@@ -6,13 +6,16 @@ export default{
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
+      <div class="collapse navbar-collapse justify-content-end" id="navbarNav">
         <ul class="navbar-nav">
           <li class="nav-item">
             <router-link class="nav-link active" aria-current="page" to="/">Home</a>
           </li>
           <li class="nav-item" v-if="role=='admin'">
             <router-link class="nav-link" to="/users">Users</a>
+          </li>
+          <li class="nav-item" v-if="role=='librarian'">
+            <router-link class="nav-link" to="/create-book">Create Book</a>
           </li>
           <li class="nav-item text-end" v-if='is_login'>
             <button class="nav-link" @click='logout'>Logout</button>
